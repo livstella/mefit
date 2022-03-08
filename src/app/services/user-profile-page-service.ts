8ed857e851c41941611d8b7d47f1bfab571fd891
Liv Reinhold <livreinhold@gmail.com>
2022-03-08 14:33:53 +0100

    Added user-profile-service with basic get on /user. Added User model

---
+++ src/app/services/user-profile-page-service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class userProfileService {
+  private user: User[] = [];
+  private error: string = '';
+
+  constructor(private readonly http: HttpClient) {}
+
+  //Get request on /user. Assume it will get all users, therefore I am using and array of users. 
+  //Should be updated to fetch one user where id=sessionStorage.getItem(currentUser)
+  public fetchUser(): void {
+    this.http
+      .get<User[]>('https://mefitbackend-ajlm.herokuapp.com/user')
+      .subscribe(
+        (user) => {
+          this.user = user;
+        },
+        (error: HttpErrorResponse) => {
+          this.error = error.message;
+        }
+      );
+  }
+}
