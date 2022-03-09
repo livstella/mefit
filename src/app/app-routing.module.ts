d2009672b2eb088ea95f8ce41b4650ba8dc8ef6f
Michel Saremi <michel.saremi@outlook.dk>
2022-03-09 14:20:54 +0100

    Added a button to take user from profile to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
-
+import { UserProfilePageComponent } from './components/user-profile-page/user-profile-page.component';
 
 const routes: Routes = [
 {path:'', redirectTo:'login', pathMatch:'full'},
@@ -11,6 +11,7 @@ const routes: Routes = [
 
 {path: 'register', component: RegistrationPageComponent ,pathMatch:'full'},
 
+{path: 'profile', component: UserProfilePageComponent ,pathMatch:'full'},
 ];
 
 @NgModule({
