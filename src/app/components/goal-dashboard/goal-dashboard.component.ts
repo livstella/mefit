b9c8aa85919691d852c8c51f6c0f944ce40d886d
Michel Saremi <michel.saremi@outlook.dk>
2022-03-09 18:54:14 +0100

    Added workout functionality, no more than 5 excersizes per session

--- src/app/components/goal-dashboard/goal-dashboard.component.ts
+++ src/app/components/goal-dashboard/goal-dashboard.component.ts
@@ -12,44 +12,71 @@ export class GoalDashboardComponent implements OnInit {
   username: string = "Michel"; //placeholder for username
   date = new Date();
   excersize_options: string[] = [];
-  choice_list: string[] = [];
-  finish_list: string[] = [];
+  ex_choice_list: string[] = [];
+  ex_finish_list: string[] = [];
+  workout_options = new Map();
+  workout_keys: string[]|any = [] ;
+  workout_keys2: string[]|any = [];
+  workout_key: string | undefined;
+  workout_ex: string[] = [];
+
 
   constructor() { }
 
   ngOnInit(): void {
-    this.excersize_options.push("Choose an excerize")
-    this.excersize_options.push("lower legs")
-    this.excersize_options.push("upper legs")
-    this.excersize_options.push("lower arm")
-    this.excersize_options.push("upper arm")
-    this.excersize_options.push("lower back")
-    this.excersize_options.push("upper back")
-    this.excersize_options.push("stomach")
-    this.excersize_options.push("chest")
+    this.excersize_options.push("Choose an excerize");
+    this.excersize_options.push("lower legs");
+    this.excersize_options.push("upper legs");
+    this.excersize_options.push("lower arms");
+    this.excersize_options.push("upper arms");
+    this.excersize_options.push("lower back");
+    this.excersize_options.push("upper back");
+    this.excersize_options.push("stomach");
+    this.excersize_options.push("chest");
 
+    this.workout_options.set(["Choose a workout"],null);
+    this.workout_options.set("legs",["lower legs","upper legs","lower back","upper back"]);
+    this.workout_options.set("arms",["lower arms","upper arms","stomach","chest"]);
+
+    for (let key of this.workout_options.keys()){
+      this.workout_keys2.push(key);
+    }
+    
   }
   //---when picking excersize from dropdown-list display 
-  onChange(){
-    let choice = $("select[name='select'] option:selected").index();
-    if (this.choice_list.length<5 && choice != 0){
-    this.choice_list.push(this.excersize_options[choice]);
-    }else if (this.choice_list.length>=5){
+  onChangeEx(){
+    let choiceEx = $("select[name='select1'] option:selected").index();
+    if (this.ex_choice_list.length<5 && this.ex_finish_list.length< 5 && choiceEx != 0){
+    this.ex_choice_list.push(this.excersize_options[choiceEx]);
+    }else if (this.ex_choice_list.length>=5){
       alert("You cannot choice more than 5 exercizes per session!");
     }
   }
 
+  //---when picking workout from dropdown-list display 
+  onChangeWork(){
+    let choiceWork = $("select[name='select2'] option:selected").index();
+    this.workout_key = this.workout_keys2[choiceWork];
+    this.workout_ex = this.workout_options.get(this.workout_key);
+    if (this.ex_choice_list.length< 4 && this.ex_finish_list.length< 4 && choiceWork != 0){
+      for (let i = 0; i<this.workout_ex.length; i++){
+        this.ex_choice_list.push(this.workout_ex[i]);}
+    }else if (this.ex_choice_list.length>=4){
+      alert("You have to many excersizes in this session to add another workout!, keep it under 6 excersizes per session!");
+    }
+  }
+
+
   //---add excersize to finish list
-  updateFinish(id:number){
-    this.finish_list.push(this.choice_list[id]);
-    this.choice_list.splice(id,1);
+  updateExFinish(id:number){
+    this.ex_finish_list.push(this.ex_choice_list[id]);
+    this.ex_choice_list.splice(id,1);
   }
 
   //---add excersize to planed list
-  updatePlaned(id:number){
-    this.choice_list.push(this.finish_list[id]);
-    this.finish_list.splice(id,1);
+  updateExPlaned(id:number){
+    this.ex_choice_list.push(this.ex_finish_list[id]);
+    this.ex_finish_list.splice(id,1);
   }
-
 
 }
