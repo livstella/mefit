4b42826f22f32a27b7925f146d4a977379d69f7c
Liv Reinhold <livreinhold@gmail.com>
2022-03-10 11:13:10 +0100

    Added routing for exercise-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
-
+import { ExercisePageComponent } from './exercise-page/exercise-page.component';
 
 const routes: Routes = [
-{path:'', redirectTo:'login', pathMatch:'full'},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 
-{path: 'login', component: LoginPageComponent ,pathMatch:'full'},
+  { path: 'login', component: LoginPageComponent, pathMatch: 'full' },
 
-{path: 'register', component: RegistrationPageComponent ,pathMatch:'full'},
+  { path: 'register', component: RegistrationPageComponent, pathMatch: 'full' },
 
+  { path: 'exercises', component: ExercisePageComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -18,4 +19,4 @@ const routes: Routes = [
   exports: [RouterModule],
   providers: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
