9f013c792df7a3c1d9242a6e9807ec980c626060
Liv Reinhold <livreinhold@gmail.com>
2022-03-16 15:35:21 +0100

    fixed embeded video by sanitizing the url using angular DomSanitizer

--- src/app/components/exercise-page/exercise-page.component.ts
+++ src/app/components/exercise-page/exercise-page.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { SelectedExerciseService } from 'src/app/services/selected-exercise.service';
 import { Exercise } from '../../models/exercise.model';
 import { exercisePageService } from '../../services/exercise-page.service';
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl,
+} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-exercise-page',
@@ -9,10 +14,13 @@ import { exercisePageService } from '../../services/exercise-page.service';
   styleUrls: ['./exercise-page.component.css'],
 })
 export class ExercisePageComponent implements OnInit {
+  public sanitizedUrl: SafeResourceUrl | null = null;
+
   //Injects the services needed
   constructor(
     private readonly exercisePageService: exercisePageService,
-    private readonly selectedExerciseService: SelectedExerciseService
+    private readonly selectedExerciseService: SelectedExerciseService,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnInit(): void {
@@ -23,8 +31,12 @@ export class ExercisePageComponent implements OnInit {
     return this.exercisePageService.exercise();
   }
 
-  onExerciseClicked(exercise:Exercise):void{
-
+  onExerciseClicked(exercise: Exercise): void {
     this.selectedExerciseService.setExercise(exercise);
+
+    this.selectedExerciseService.setSafeVideo(
+      this.sanitizer.bypassSecurityTrustResourceUrl(exercise.videoLink)
+    );
+    console.log(exercise);
   }
 }
--- src/app/components/selected-exercise/selected-exercise.component.ts
+++ src/app/components/selected-exercise/selected-exercise.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Exercise } from 'src/app/models/exercise.model';
 import { SelectedExerciseService } from 'src/app/services/selected-exercise.service';
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl,
+} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-selected-exercise',
   templateUrl: './selected-exercise.component.html',
-  styleUrls: ['./selected-exercise.component.css']
+  styleUrls: ['./selected-exercise.component.css'],
 })
-export class SelectedExerciseComponent  {
+export class SelectedExerciseComponent {
+  sanitizedVideo: SafeResourceUrl | null = null;
 
-  constructor(private readonly selectedExerciseService:SelectedExerciseService) { }
+  constructor(
+    private readonly selectedExerciseService: SelectedExerciseService,
+    private sanitizer: DomSanitizer
+  ) {}
 
- get exercise(): Exercise|null{
-   return this.selectedExerciseService.exercise()
- }
+  get exercise(): Exercise | null {
+    return this.selectedExerciseService.exercise();
+  }
+
+  get safeVideo(): SafeResourceUrl | null {
+    return this.selectedExerciseService.safeVideo();
+  }
 }
